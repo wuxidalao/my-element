238ff8ae33e8f32a79ab48bc3f416c2c233b3e8e
wuxidalao <yatouxixi520@qq.com>
2019-05-27 17:31:05 +0800

    1

---
+++ src/components/bottom.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>信息查询系统</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-container>
+      <el-header class="topContainer">
+        <div class="topLogo"><img src="../assets/logo.png" /></div>
+        <div class="topNav"><top></top></div>
+      </el-header>
+      <el-container :style="height">
+        <el-aside width="200px" class="leftContainer"><left></left></el-aside>
+        <el-container>
+          <el-main class="contentContainer">
+            <breadcrumb></breadcrumb>
+            <tab></tab>
+          </el-main>
+          <el-footer class="bottomContainer"><bottom></bottom></el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import top from './top'
+import left from './left'
+import breadcrumb from './breadcrumb'
+import tab from './tab'
+import bottom from './bottom'
+
+export default {
+  name: 'index',
+  components: {
+    'top': top,
+    'left': left,
+    'breadcrumb': breadcrumb,
+    'tab': tab,
+    'bottom': bottom
+  },
+  data () {
+    return {
+      height: {
+        height: ''
+      }
+    }
+  },
+  created () {
+    this.hh()
+  },
+  methods: {
+    hh () {
+      this.height.height = window.innerHeight - 60 + 'px'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.topContainer{
+  background-color: rgb(84, 92, 100);
+  display: flex;
+  justify-content:space-between;
+}
+
+.topLogo>img{
+  width: 100px;
+  height: 60px;
+}
+
+.leftContainer{
+
+}
+
+.bottomContainer{
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  background-color: rgb(245, 245, 245);
+}
+</style>
